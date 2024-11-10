7214b0c2711636fb68e306239ee96377dfce7a5d
Kaan <133776954+KaanKocaturkmen@users.noreply.github.com>
2024-11-11 00:06:35 +0300

    Add files via upload

---
+++ Assets/Script/app.js
@@ -0,0 +1,56 @@
+const audioPlayer = document.getElementById('audioPlayer');
+const selectedMusic = document.getElementById('selectedMusic');
+
+$(document).on("click", "td", function () {
+    const data = `${Number($(this).data("x"))} ${Number($(this).data("y"))}`
+    $("#cordinateData").html(data);
+    $("#timeData").html(pauseMusic());
+    $("#songData").html($("#mediaPlayer .body .selected").text())
+});
+
+$(document).on("click", "#expandMediaBtn", function () {
+    $("#mediaPlayer").slideToggle('fast', () => {
+        if ($("#mediaPlayer").css('display') === 'block') {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+    });
+});
+
+$(document).on("click", "#expandData", function () {
+    $("#dataContainer").slideToggle('fast', () => {
+        if ($("#dataContainer").css('display') === 'block') {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+    });
+});
+
+$(document).on("click", ".media-track button", function () {
+    $("#selectedMusic").text($(this).attr('value'))
+});
+
+function selectSong(songFile, elem) {
+    audioPlayer.src = `./Assets/Media/${songFile}`;
+    selectedMusic.textContent = songFile.replace('_', ' ');
+    audioPlayer.currentTime = 0;
+    $(".media-track button").removeClass('selected');
+    $(elem).addClass('selected');
+    playMusic();
+}
+
+function playMusic() {
+    audioPlayer.play();
+}
+
+function pauseMusic() {
+    audioPlayer.pause();
+    const currentTime = audioPlayer.currentTime;
+    return `${Math.floor(currentTime / 60)}:${Math.floor(currentTime % 60)}`
+}
+
+audioPlayer.onended = () => {
+    console.log("Şarkı bitti.");
+};
