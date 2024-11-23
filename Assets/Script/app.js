077b96547d5cfd4832fa60c10ee9053e426cf5e3
Kaan <kaan.kocaturkmen@efectura.com>
2024-11-24 01:23:58 +0300

    CordinateChangedResponsiveDeleted

--- Assets/Script/app.js
+++ Assets/Script/app.js
@@ -6,6 +6,7 @@ $(document).on("click", "td", function () {
     $(".cordinate-data").html(data);
     $(".time-data").html(pauseMusic());
     $(".song-data").html($("#mediaPlayer .body .selected").text())
+    console.log(data)
 });
 
 $(document).on("click", "#expandMediaBtn", function () {
@@ -56,5 +57,38 @@ audioPlayer.onended = () => {
 };
 
 $(document).ready(function () {
-    $("#myModal").modal('show');
+    $("#userName").on("change input", function () {
+        const nameValue = $(this).val();
+        if (nameValue.length > 0) {
+            $("#confirmName").attr('disabled', false);
+        } else {
+            $("#confirmName").attr('disabled', true);
+        }
+    });
+    
+    // $("#myModal").modal('show');
+
+    $(".download-data").on("click", function () {
+        let data = [
+            {Name: $("#userName").val(), Song: $("span.song-data").text().trim(), Duration: $("span.time-data").html().trim(), Cordiante: $("span.song-data").text().trim()},
+        ];
+
+        function convertToCSV(json) {
+            const header = Object.keys(json[0]);
+            const rows = json.map(obj => header.map(fieldName => JSON.stringify(obj[fieldName], null, 0)).join(','));
+            return [header.join(','), ...rows].join('\r\n');
+        }
+
+        const csv = convertToCSV(data);
+
+        // CSV verisini Blob olarak oluştur ve indir
+        const blob = new Blob([csv], {type: 'text/csv;charset=utf-8;'});
+        const link = $("<a>")
+            .attr("href", URL.createObjectURL(blob))
+            .attr("download", "data.csv")
+            .appendTo("body");
+        link[0].click();
+        link.remove();
+    });
+    
 });
